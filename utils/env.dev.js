5fe66f8d78b03b576504fb27830208a4bc836d43
Jiahua <edures@protonmail.com>
2022-06-07 01:17:47 +0800

    Deploy for staging

--- utils/env.dev.js
+++ utils/env.dev.js
@@ -1,53 +1,54 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 /**
  * Setup environment variables for app configuration
  */
 dotenv.config();
 
+const nodeEnv = process.env.NODE_ENV;
 const audience = process.env.AUTH0_AUDIENCE;
 const domain = process.env.AUTH0_DOMAIN;
 const serverPort = process.env.PORT;
-const clientOriginUrl = process.env.CLIENT_ORIGIN_URL;
+const clientOriginUrl = nodeEnv === 'development' ? process.env.CLIENT_ORIGIN_URL : 'https://client-stonks.vercel.app/';
 const databaseURI = process.env.DB_URI;
 
 if (!audience) {
-    throw new Error(
-        '.env is missing the definition of an AUTH0_AUDIENCE environmental variable'
-    );
+  throw new Error(
+    '.env is missing the definition of an AUTH0_AUDIENCE environmental variable'
+  );
 }
 
 if (!domain) {
-    throw new Error(
-        '.env is missing the definition of an AUTH0_DOMAIN environmental variable'
-    );
+  throw new Error(
+    '.env is missing the definition of an AUTH0_DOMAIN environmental variable'
+  );
 }
 
 if (!serverPort) {
-    throw new Error(
-        '.env is missing the definition of a API_PORT environmental variable'
-    );
+  throw new Error(
+    '.env is missing the definition of a API_PORT environmental variable'
+  );
 }
 
 if (!clientOriginUrl) {
-    throw new Error(
-        '.env is missing the definition of a APP_ORIGIN environmental variable'
-    );
+  throw new Error(
+    '.env is missing the definition of a APP_ORIGIN environmental variable'
+  );
 }
 
 if (!databaseURI) {
   throw new Error(
-      '.env is missing the definition of a DB_URI environmental variable'
+    '.env is missing the definition of a DB_URI environmental variable'
   );
 }
 
-const clientOrigins = ['http://localhost:3000'];
+const clientOrigins = nodeEnv === 'development' ? ['http://localhost:3000'] : ['https://client-stonks.vercel.app/']
 
 export {
-    audience,
-    domain,
-    serverPort,
-    clientOriginUrl,
-    clientOrigins,
-    databaseURI
+  audience,
+  domain,
+  serverPort,
+  clientOriginUrl,
+  clientOrigins,
+  databaseURI,
 };
