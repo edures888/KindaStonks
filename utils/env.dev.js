00f7325b20e00626d3c3154785a9105cb992fd80
Jiahua <edures@protonmail.com>
2022-06-07 01:29:27 +0800

    Deploy for staging2

--- utils/env.dev.js
+++ utils/env.dev.js
@@ -24,12 +24,6 @@ if (!domain) {
   );
 }
 
-if (!serverPort) {
-  throw new Error(
-    '.env is missing the definition of a API_PORT environmental variable'
-  );
-}
-
 if (!clientOriginUrl) {
   throw new Error(
     '.env is missing the definition of a APP_ORIGIN environmental variable'
