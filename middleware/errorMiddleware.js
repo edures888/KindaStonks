d177bb7e0effcebde566cf9311ccd2db71a78e66
Jiahua <edures@protonmail.com>
2022-07-18 17:41:33 +0800

    Fix bugs, functional active assets

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -2,7 +2,9 @@
  * Simple error handler middleware
  */
 const errorMiddleware = (err, req, res, next) => {
-  res.status(err.status || 500).send(`${err.name}: ${err.message} `);
+  res
+    .status(err.status || 500)
+    .send(`Error Middleware, ${err.name}: ${err.message} `);
 };
 
 export default errorMiddleware;
